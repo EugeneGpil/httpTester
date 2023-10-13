ade7510ec94b896e18abcc58939aa7733e40656d
EugeneGpil <gpilligrim@gmail.com>
2023-10-13 05:37:02 +0000

    1

---
+++ app/modules/Request/tests/should_return_request_with_correct_request_body/should_return_request_with_correct_request_body_test.go
@@ -0,0 +1,31 @@
+package should_return_request_with_correct_request_body
+
+import (
+	"testing"
+
+	"github.com/EugeneGpil/httpTester/app/modules/Request"
+	"github.com/EugeneGpil/httpTester/app/modules/Request/dto"
+	"github.com/EugeneGpil/tester"
+)
+
+func Test_should_return_request_with_correct_request_body(t *testing.T) {
+	tester.SetTester(t)
+
+	getRequestBody := struct {
+		Test string
+	}{
+		Test: "test",
+	}
+
+	request := Request.GetRequest(dto.GetRequestDto{
+		Method: "test",
+		Url:    "test",
+		Body:   getRequestBody,
+	})
+
+	resultRequestBody := make([]byte, 15)
+
+	request.Body.Read(resultRequestBody)
+
+	tester.AssertSame("{\"Test\":\"test\"}", string(resultRequestBody))
+}
